28f2a1de132fc19d4ef7570f867f961d68cc71c0
Jessica Ho <Jschobui@gmail.com>
2023-02-09 23:01:50 -0800

    Storage Tests add method

---
+++ Logger.Tests/StorageTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger;
+
+[TestClass]
+public class StorageTests
+{
+    private class Entity : IEntity
+    {
+        public Guid Id { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+
+        public string Name => throw new NotImplementedException();
+    }
+    [TestMethod]
+    public void AddTest()
+    {
+        var storage = new Storage();
+        var entity = new Entity { Id = Guid.NewGuid() };
+
+        storage.Add(entity);
+
+        Assert.IsTrue(storage.Contains(entity));
+    }
+
+}
